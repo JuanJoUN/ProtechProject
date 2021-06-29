99860f491c06261af79b407ab67fd68fbd8395e9
JuanJoUN <jusalazarc@unal.edu.co>
2021-06-28 22:27:56 -0500

    Actualización vistas

--- src/app/routes/main.js
+++ src/app/routes/main.js
@@ -13,4 +13,7 @@ module.exports = app => {
         res.render('../views/productos.ejs');
     });
 
+    app.get('/aggProductos',(req,res)=>{
+        res.render('../views/aggProductos');
+    });
 }
