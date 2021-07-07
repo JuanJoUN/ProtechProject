8ead588a3f27e2790eeeddbf719925ffe5d4f509
JuanJoUN <jusalazarc@unal.edu.co>
2021-07-06 22:41:56 -0500

    Add register, login

--- src/app/routes/main.js
+++ src/app/routes/main.js
@@ -5,6 +5,24 @@ module.exports = app => {
         res.render('../views/register');
     });
 
+    app.get('/', (req, res)=>{
+
+        if (req.session.loggedin){
+            connection.query('SELECT * FROM producto',(error, results)=>{
+                if(error){
+                    res.send(error);
+                }else{
+                    res.render('../views/productos.ejs',{
+                        product: results
+                    })
+                }
+            })
+
+        }else{
+            res.render('../views/login');
+        }
+    });
+
     app.get('/login', (req, res)=>{
         res.render('../views/login');
     });
@@ -19,5 +37,38 @@ module.exports = app => {
 
     app.get('/usuarios',(req,res)=>{
         res.render('../views/usuarios');
+    });
+
+    app.post('/loginAuth', async (req,res)=>{
+        const {usuario,password} = req.body;
+        if (usuario && password){
+            console.log(usuario,password)
+            connection.query('SELECT * FROM usuario WHERE usuario = ?', [usuario], async(error, results)=>{
+
+                if (results.length ===0 || !(password===results[0].contraseña)){
+                    res.render('../views/login.ejs',{
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "Usuario y/o contraseña incorrectos",
+                        alertIcon: "error",
+                        showConfirmButton: true,
+                        timer: 1500,
+                        ruta: ''
+                    })
+                }else{
+                    req.session.loggedin=true;
+                    req.session.name = results[0].nombreUsuario;
+                    res.render('../views/login.ejs', {
+                        alert: true,
+                        alertTitle:"Inicio de sesión",
+                        alertMessage: "Inicio de sesión exitoso",
+                        alertIcon: "success",
+                        showConfirmButton: false,
+                        timer: 1500,
+                        ruta: ''
+                    });
+                }
+            })
+        }
     });
 }
