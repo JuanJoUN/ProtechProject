231e6304895455f2cc07d3e366a1fdb436067d9f
JuanJoUN <jusalazarc@unal.edu.co>
2021-08-10 16:22:07 -0500

    db changes 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "bf934f5528948e",
+    database: "aa8c2759",
+    password: "aa8c2759"
 });
 
 connection.connect((error)=>{
