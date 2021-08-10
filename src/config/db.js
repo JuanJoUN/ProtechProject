b9e46741aecc82866932f0c6b6b334c319b407e0
JuanJoUN <jusalazarc@unal.edu.co>
2021-08-10 17:34:16 -0500

    Revert "DB Ch"
    This reverts commit 1956cfada7810a09ebdb3fcd5f33f249dcb9724a.

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,39 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: "us-cdbr-east-04.cleardb.com",
+//     user: "bf934f5528948e",
+//     database: "heroku_54b881ededb837f",
+//     password: "aa8c2759"
+// });
+
+
+const connection_BD = {
     host: "us-cdbr-east-04.cleardb.com",
     user: "bf934f5528948e",
     database: "heroku_54b881ededb837f",
     password: "aa8c2759"
-});
+};
 
-connection.connect((error)=>{
-    if(error){
-        console.log("Error: " + error);
+function handleDisconnect(connection_BD){
+    connectionn = mysql.createPool(connection_BD);
+    connectionn.getConnection(function (error){
+        if (error){
+            console.log("Error while connecting to db: ", error);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
 
-    }else{
-        console.log("Conexión a la BD exitosa")
-    }
-});
+    connectionn.on('error', function (error){
+        console.log('Database error', error);
+        if (error.code === 'PROTOCOL_CONNECTION_LOST'){
+            handleDisconnect();
+        }else{
+            throw error;
+        }
+    });
+}
 
-module.exports = connection;
+handleDisconnect(connection_BD);
+
 
